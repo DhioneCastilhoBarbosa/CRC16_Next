1b20cc747d8ee1845bbc7914d5fce8bd81ce2466
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2023-05-04 16:32:43 -0300

    insert page convert hex

--- pages/hexpage.tsx
+++ pages/hexpage.tsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react'
-import { Inter } from 'next/font/google'
-import { crc16 } from 'easy-crc'
 import Link from 'next/link'
 
-
-const inter = Inter({ subsets: ['latin'] })
-
-
-export default function Home() {
-  const [codigo, setCodigo] = useState('')
+export default function HexConvert() {
+  const[Hex,setHex] = useState('')
   const [valor, setValor] = useState('');
 
   const handleChange = (event: any) => {
 
     setValor((event.target.value).toUpperCase());
+    
+  }
+
+  const decimalToHex =()=> {
+    let padding =0;
+    
+    let hex = Number(valor).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+      hex = "0" + hex;
+    }
+
+    let tamanho = 2;
+    let palavras = [];
+    for (let i = 0; i < hex.length; i += tamanho) {
+      palavras.push(hex.slice(i, i + tamanho));
+    }
+
+    setHex( palavras.reverse().join('').toUpperCase());
 
   }
 
-
-
-  const handleClick = () => {
-    console.log(valor)
-    let checksum = ((crc16('MODBUS', valor)).toString(16).padStart(4, '0')).toUpperCase()
-    valor == "" ? setCodigo("") : setCodigo(checksum);
-    //console.log(checksum );
-
-  }
-
-
-  
-
-  
-
-  return (
+  return(
+   
     <main className="flex min-h-screen flex-col items-center justify-between ">
 
       <header className='w-full  bg-blue-700'>
@@ -44,21 +44,20 @@ export default function Home() {
               Decodificar
             </Link>
             <Link  className='hover:text-cyan-300 duration-700' href="/hexpage">
-            Converter
+              Converter
             </Link>
           </div>
-
         </div>
       </header>
 
-      <div className='flex flex-col  bg-blue-400 px-24 pb-24 pt-4 rounded-2xl shadow-2xl shadow-gray-600 items-center'>
-        <p className='mb-16 mt-4 font-bold text-xl text-white'>Insira o código para decodificação</p>
+      <div className='flex flex-col  bg-violet-500 px-24 pb-24 pt-4 rounded-2xl shadow-2xl shadow-gray-600 items-center'>
+        <p className='mb-16 mt-4 font-bold text-xl text-white'>Insira o código decimal para converter para hexdecimal</p>
         <div>
-          <input className='w-72 p-1 rounded-lg text-center border-2 border-blue-400 focus:outline-none' type="text" onChange={handleChange} />
-          <button className=' ml-2  px-4 py-1 bg-blue-600 rounded-lg text-white font-bold hover:bg-blue-800 duration-700' onClick={handleClick}>Enviar</button>
+          <input className='w-72 p-1 rounded-lg text-center border-2 border-violet-400 focus:outline-none' type="text"  onChange={handleChange}/>
+          <button className=' ml-2  px-4 py-1 bg-blue-600 rounded-lg text-white font-bold hover:bg-blue-800 duration-700' onClick={decimalToHex} >Enviar</button>
         </div>
 
-        <p className=' mt-8 font-bold text-xl text-white' >{valor === 'FFFF' ? "" : codigo}</p>
+        <p className=' mt-8 font-bold text-xl text-white' >{Hex === "0"? "" : Hex}</p>
 
       </div>
 
@@ -72,7 +71,6 @@ export default function Home() {
         </div>
       </footer>
     </main>
-
-
   )
+  
 }
